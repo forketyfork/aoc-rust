decd4c80cc38aaf653ab1c00ac15f96732d1e06c
Forketyfork <forketyfork@icloud.com>
2025-12-25 16:05:20 +0100

    Skip tests on real data in GitHub Actions

--- tests/day2024_03.rs
+++ tests/day2024_03.rs
@@ -1,5 +1,6 @@
 use adv_code_2024::{day2024_03, read_input};
 
+#[cfg_attr(ci_environment, ignore)]
 #[test]
 fn day2024_03_real_input() -> anyhow::Result<()> {
     let input = read_input("2024-03")?;
--- tests/day2024_05.rs
+++ tests/day2024_05.rs
@@ -1,5 +1,6 @@
 use adv_code_2024::{day2024_05, read_input};
 
+#[cfg_attr(ci_environment, ignore)]
 #[test]
 fn day2024_05_real_input() -> anyhow::Result<()> {
     let input = read_input("2024-05")?;
